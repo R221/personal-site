76d7ed969d883676cb10690dd3c140a1e7f96c3d
R221 <rcjames1111@gmail.com>
2021-10-26 19:47:05 +0900

    Added Projects component and CSS items

---
+++ personal-site/ClientApp/src/app/projects/projects.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.scss']
+})
+/** projects component*/
+export class ProjectsComponent {
+    /** projects ctor */
+    constructor() {
+
+    }
+}
